fc675076ce8d2dd83615b787536c89f3066b7126
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-04-22 19:13:49 +0700

    [chore] configure ESLint and add Prettier settings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +10,66 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: [
+      'next',
+      'next/core-web-vitals',
+      'next/typescript',
+      'plugin:prettier/recommended',
+      'plugin:jsx-a11y/recommended',
+      'plugin:import/recommended',
+      'plugin:import/typescript',
+      'plugin:react-hooks/recommended',
+    ],
+    plugins: [
+      'prettier',
+      'jsx-a11y',
+      'import',
+      'react-hooks',
+      '@typescript-eslint',
+    ],
+    rules: {
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'all',
+          semi: true,
+          singleQuote: true,
+          endOfLine: 'auto',
+        },
+        {
+          usePrettierrc: true,
+        },
+      ],
+
+      // React
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+
+      'jsx-a11y/alt-text': 'error',
+      'jsx-a11y/aria-props': 'error',
+      'jsx-a11y/aria-proptypes': 'error',
+      'jsx-a11y/aria-unsupported-elements': 'error',
+      'jsx-a11y/role-has-required-aria-props': 'error',
+      'jsx-a11y/role-supports-aria-props': 'error',
+      'jsx-a11y/click-events-have-key-events': 'error',
+      'jsx-a11y/no-noninteractive-element-interactions': 'error',
+      'jsx-a11y/anchor-is-valid': 'error',
+
+      // TypeScript
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  }),
 ];
 
 export default eslintConfig;
