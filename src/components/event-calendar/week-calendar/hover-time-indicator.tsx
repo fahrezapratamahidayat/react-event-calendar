44f80b99505c73d2bfe4f0b1dd51d13c8c3e9b58
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-04-24 21:36:57 +0700

    [feat] add HoverTimeIndicator component to display hover time in week calendar view

---
+++ src/components/event-calendar/week-calendar/hover-time-indicator.tsx
@@ -0,0 +1,28 @@
+import { TimeFormatType } from '@/hooks/use-event-calendar';
+import { formatTimeDisplay } from '@/lib/date-fns';
+
+interface HoverTimeIndicatorProps {
+  hour: number;
+  minute: number;
+  hourHeight: number;
+  timeFormat: TimeFormatType;
+}
+export const HoverTimeIndicator = ({
+  hour,
+  minute,
+  timeFormat,
+  hourHeight,
+}: HoverTimeIndicatorProps) => {
+  return (
+    <div
+      className="pointer-events-none absolute right-0 left-0 z-50 border-t-2 border-blue-400"
+      style={{
+        top: `${hour * hourHeight + (minute / 60) * hourHeight}px`,
+      }}
+    >
+      <div className="absolute -top-6 left-0 rounded-md bg-blue-400 px-2 py-0.5 text-xs text-white shadow-sm">
+        {formatTimeDisplay(hour, minute, timeFormat)}
+      </div>
+    </div>
+  );
+};
