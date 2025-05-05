cd9f47f472b1043461b0e507eeaddde2473ae745
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-05-05 21:18:45 +0700

    [refactor] adjust width of SearchDayPicker component for improved layout consistency and user experience

--- src/components/event-calendar/ui/search-day-picker.tsx
+++ src/components/event-calendar/ui/search-day-picker.tsx
@@ -130,7 +130,7 @@ export function SearchDayPicker({
           role="combobox"
           aria-expanded={open}
           className={cn(
-            'w-[180px] justify-between font-normal',
+            'w-[130px] justify-between font-normal',
             !selectedDay && 'text-muted-foreground',
             className,
           )}
@@ -152,7 +152,7 @@ export function SearchDayPicker({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0" align="start">
+      <PopoverContent className="w-[130px] p-0" align="start">
         <Command shouldFilter={false}>
           <CommandInput
             placeholder="Cari hari..."
