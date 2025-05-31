ecdd453af8b0dd94617d168cf09d3210882154d4
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-05-31 18:29:34 +0700

    [feat] enhance search parameters schema by adding new fields for improved event filtering and search functionality

--- src/lib/searchParams.ts
+++ src/lib/searchParams.ts
@@ -1,5 +1,7 @@
+import { CalendarViewType } from '@/types/event';
 import {
   createSearchParamsCache,
+  parseAsArrayOf,
   parseAsInteger,
   parseAsIsoDate,
   parseAsString,
@@ -7,8 +9,13 @@ import {
 
 export const searchParamsCache = createSearchParamsCache({
   date: parseAsIsoDate.withDefault(new Date()),
-  view: parseAsString.withDefault('month'),
+  view: parseAsString.withDefault(CalendarViewType.MONTH),
   title: parseAsString.withDefault(''),
-  category: parseAsString.withDefault(''),
+  categories: parseAsArrayOf(parseAsString).withDefault([]),
   daysCount: parseAsInteger.withDefault(7),
+  search: parseAsString.withDefault(''),
+  colors: parseAsArrayOf(parseAsString).withDefault([]),
+  locations: parseAsArrayOf(parseAsString).withDefault([]),
+  repeatingTypes: parseAsArrayOf(parseAsString).withDefault([]),
+  isRepeating: parseAsString.withDefault(''),
 });
