029ca6ee6e0216f067900b69c7b2afc30fd98bea
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-06-13 21:12:23 +0700

    [feat] add DocsProvider component to structure documentation layout with integrated Navbar, DocSidebar, and TableOfContents for improved user navigation

---
+++ src/components/docs/docs-provider.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import {
+  TableOfContents,
+  TableOfContentsMinimal,
+} from '@/components/docs/table-of-contents';
+import { DocSidebar } from '@/components/docs/doc-sidebar';
+import Navbar from '../navbar';
+
+export function DocsProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-background min-h-screen">
+      <Navbar />
+      <div className="container mx-auto">
+        <div className="flex">
+          <div className="hidden w-64 shrink-0 border-r md:block">
+            <div className="sticky top-16 h-[calc(100vh-4rem)]">
+              <DocSidebar />
+            </div>
+          </div>
+          <main className="flex-1">
+            <div className="mx-auto w-full px-4 py-10">
+              <div className="grid grid-cols-1 gap-12 lg:grid-cols-[1fr_220px]">
+                <div className="min-w-0">
+                  <div className="mx-auto flex w-full max-w-2xl flex-col gap-8 py-6 text-neutral-800 dark:text-neutral-300">
+                    {children}
+                  </div>
+                </div>
+                <div className="hidden lg:block">
+                  <div className="sticky top-24">
+                    <TableOfContentsMinimal />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
+}
