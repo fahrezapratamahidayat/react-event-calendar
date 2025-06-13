e46d15e7b3bfade8d167d484fd92234d416e9dc4
fahrezapratamahidayat <fahrezaxgamer@gmail.com>
2025-06-13 21:10:47 +0700

    [refactor] replace Sidebar and Breadcrumb components with DocsProvider to simplify layout structure and enhance maintainability

--- src/app/docs/layout.tsx
+++ src/app/docs/layout.tsx
@@ -1,47 +1,10 @@
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator,
-} from '@/components/ui/breadcrumb';
-import { Separator } from '@/components/ui/separator';
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from '@/components/ui/sidebar';
-import { AppSidebar } from '@/components/app-sidebar';
+'use client';
+import { DocsProvider } from '@/components/docs/docs-provider';
 
 export default function DocsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <header className="sticky top-0 flex h-16 shrink-0 items-center gap-2 border-b">
-          <div className="flex h-full items-center gap-2 px-3">
-            <SidebarTrigger />
-            <Separator orientation="vertical" className="mr-2 h-4" />
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="/docs">Dokumentasi</BreadcrumbLink>
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>React Event Calendar</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
-          </div>
-        </header>
-        <main className="mx-auto p-4 lg:p-8">{children}</main>
-      </SidebarInset>
-    </SidebarProvider>
-  );
+  return <DocsProvider>{children}</DocsProvider>;
 }
